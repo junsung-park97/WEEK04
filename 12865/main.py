fc8da60d61d78965b01050f50dc323c0fd85d219
junSung <not971006@gmail.com>
2025-08-02 15:06:03 +0900

    백준 12856번 -냅색문제-

---
+++ 12865/main.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().rstrip().split())
+item = []
+# 물건들 정보 저장
+weights = [0]  # 1-based 인덱스를 위해 0 추가
+values = [0]
+
+
+for i in range(n):
+    w, v = map(int, input().rstrip().split())
+    weights.append(w)
+    values.append(v)
+    
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+for i in range(n + 1):
+    for w in range(k + 1):
+        # i번째 물건을 선택하지 않는 경우
+        dp[i][w] = dp[i-1][w]
+        
+        # i번째 물건을 선택하는 경우 (무게 제한 확인)
+        # dp[i - 1][w - weights[i]]
+        if weights[i] <= w:
+            dp[i][w] = max(dp[i][w], dp[i-1][w - weights[i]] + values[i])
+
+print(dp[n][k])
+
