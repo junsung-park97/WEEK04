35a5f33121c2029022189669c3000f296aca579a
junSung <not971006@gmail.com>
2025-08-04 10:58:26 +0900

    백준 11047번동전문제  -그리디알고리즘-

--- 11047/main.py
+++ 11047/main.py
@@ -4,8 +4,20 @@ input = sys.stdin.readline
 
 n, k = map(int, input().rstrip().split())
 coins = []
+
 for _ in range(n):
     coin = int(input())
     coins.append(coin)
 
-print(coins)
+count = 0
+
+# 큰 동전부터 사용 (그리디)
+for i in range(n-1, -1, -1):
+    if k >= coins[i]:
+        count += k // coins[i]  # 해당 동전을 최대한 사용
+        k %= coins[i]           # 나머지 금액 계산
+
+    if k == 0:
+        break
+
+print(count)
